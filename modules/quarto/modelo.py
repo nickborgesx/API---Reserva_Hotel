3248c4d484d019344fb2c31e13f1b96f45520748
Nicolas Borges <110075681+nickborgesx@users.noreply.github.com>
2024-02-12 00:09:07 -0300

    Delete modules/quarto directory

--- modules/quarto/modelo.py
+++
@@ -1,7 +0,0 @@
-class Quarto:
-    def __init__(self, numero, capacidade, disponivel, hotel_id, id=None):
-        self.numero = numero
-        self.capacidade = capacidade
-        self.disponivel = disponivel
-        self.hotel_id = hotel_id
-        self.id = id
